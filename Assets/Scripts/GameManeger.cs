beadb407905e457cb5d8780d0e895c77d9229a13
kamera <asahi20000124@icloud.com>
2019-04-19 00:20:46 +0900

    GameManeger,Playerクラスを追加,Stageの仮modelを追加,UIのボタンを押すと画面中心にRayが出るようにした

---
+++ Assets/Scripts/GameManeger.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum State
+{
+    wait, start, isgame, end
+}
+
+public class GameManeger : MonoBehaviour
+{
+    public State GameState { get; private set; }
+    float startTime = 3, gameTime = 60, count;
+    bool isARView;//AR用の画像が移っているかの判定用のbool(いらないかも)
+    // Start is called before the first frame update
+    void Start()
+    {
+        ChangeState(State.start);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameState == State.wait)
+        {
+            //最初はwait、Stage表示後にStartへ
+            ChangeState(State.start);
+        }
+        if (GameState == State.start)
+        {
+            StartCount();
+            Debug.Log(count);
+        }
+        if (GameState == State.isgame)
+        {
+            GameTimeCount();
+        }
+    }
+
+    void ChangeState(State s)
+    {
+        GameState = s;
+    }
+
+    void StartCount()
+    {
+        if (TimeCount(ref count))
+        {
+            ChangeState(State.isgame);
+            count = gameTime;
+        }
+    }
+
+    void GameTimeCount()
+    {
+        if (TimeCount(ref count))
+        {
+            ChangeState(State.end);
+            count = startTime;
+        }
+    }
+
+    bool TimeCount(ref float time)
+    {
+        time -= Time.deltaTime;
+        if (time <= 0)
+        {
+            return true;
+        }
+        return false;
+    }
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnShot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, Mathf.Infinity))
+        {
+            if (hit.transform.gameObject.tag == "Enemy")
+            {
+                //敵への攻撃、animation呼び出し
+                Debug.Log("");
+            }
+        }
+    }
+}
