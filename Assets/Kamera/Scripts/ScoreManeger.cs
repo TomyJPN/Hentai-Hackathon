81b9dd77ef062792d1561ef40d6ed15e59612880
kamera <asahi20000124@icloud.com>
2019-04-21 16:11:29 +0900

    UIの変更、Score表示機能の追加

--- Assets/Kamera/Scripts/ScoreManeger.cs
+++ Assets/Kamera/Scripts/ScoreManeger.cs
@@ -1,10 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreManeger : MonoBehaviour
 {
-    public static int score, shotCount, hitCount;
+    public static int score;
+    public static float shotCount, hitCount;
+    [SerializeField]
+    private Text scoreText, timeText;
+    [SerializeField]
+    private GameManeger gameManeger;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,11 +20,19 @@ public class ScoreManeger : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        ScoreView();
+    }
 
+    void ScoreView()
+    {
+        scoreText.text = score.ToString();
+        timeText.text = gameManeger.GetTimeCount().ToString() + "s";
     }
 
     public static void ResetScore()
     {
         score = 0;
+        shotCount = 0;
+        hitCount = 0;
     }
 }
