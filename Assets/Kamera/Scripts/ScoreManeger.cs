bbee327542103c87fbb5d99a4bceaf4ffe855dc1
kamera <asahi20000124@icloud.com>
2019-04-21 09:50:46 +0900

    チェーンのAnimaionを調整

---
+++ Assets/Kamera/Scripts/ScoreManeger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManeger : MonoBehaviour
+{
+    public static int score, shotCount, hitCount;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public static void ResetScore()
+    {
+        score = 0;
+    }
+}
